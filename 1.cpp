8654a1edc985dbc9fe3163fa078840a8d93a06ab
mdgordillob <mdgordillob@unal.edu.co>
2017-02-15 10:01:07 -0500

    Subida de archivos 1.cpp y 2.cpp

---
+++ 1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <cmath>
+const double N=5;
+double  expansion(double n, int m){
+  int i=1;
+  double t=1;
+  while(i<=m){
+  t=t*n;
+  i++;
+  }
+  return t;
+}
+double  potencia1(double n, int  m){
+  double pot=std::pow(1-n,m);
+  return pot;
+  
+}
+double  potencia2(double n)
+
+{
+  double pot=0,x=0;
+  x=1-n;
+  pot=1-8*expansion(x,1) + 28*expansion(x,2) - 56*expansion(x,3) + 70*expansion(x,4) - 56*expansion(x,5)+ 28*expansion(x,6) - 8*expansion(x,7)+expansion(x,8); 
+ 
+}
+
+
+double  potencia3(double n){
+  double pot=0;
+  pot=1-8*pow(1-n,1) + 28*pow(1-n,2) - 56*pow(1-n,3) + 70*(1-n,4)-56*(1-n,5)+28*pow(1-n,6)-8*(1-n,7)+pow(1-n,8);
+  return pot;
+  
+}
+int main(){
+  std::cout.precision(5);
+  std::cout.setf(std::ios::scientific);
+  double f=0,por=0,h=0,k=0;
+  f=potencia1(0.1,8);
+  std::cout<<f<<std::endl;
+  h=potencia2(0.1);
+  std::cout<<h<<std::endl;
+  k=potencia3(0.1);
+  std::cout<<k;
+ 
+  
+    return 0;
+}
+
+
